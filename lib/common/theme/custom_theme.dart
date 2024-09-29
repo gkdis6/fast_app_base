e21afbeb987b0788b016a510ee4798293554c859
SangYong <gkdis6@gmail.com>
2024-09-29 20:35:51 +0900

    color seed 제거

--- lib/common/theme/custom_theme.dart
+++ lib/common/theme/custom_theme.dart
@@ -30,6 +30,8 @@ enum CustomTheme {
   }
 }
 
+// MaterialColor primarySwatchColor = Colors.lightBlue;
+
 ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -37,10 +39,11 @@ ThemeData lightTheme = ThemeData(
     // textTheme: CustomGoogleFonts.diphylleiaTextTheme(
     //   ThemeData(brightness: Brightness.light).textTheme,
     // ),
-    colorScheme: ColorScheme.fromSeed(seedColor: CustomTheme.light.appColors.seedColor));
+    colorScheme: ColorScheme.light(background: Colors.white));
 
-const darkColorSeed = Color(0xbcd5ff7e);
+// const darkColorSeed = Color(0xbcd5ff7e);
 ThemeData darkTheme = ThemeData(
+    // primarySwatch: primarySwatchColor,
     useMaterial3: true,
     visualDensity: VisualDensity.adaptivePlatformDensity,
     brightness: Brightness.dark,
@@ -48,5 +51,4 @@ ThemeData darkTheme = ThemeData(
     // textTheme: GoogleFonts.nanumMyeongjoTextTheme(
     //   ThemeData(brightness: Brightness.dark).textTheme,
     // ),
-    colorScheme: ColorScheme.fromSeed(
-        seedColor: CustomTheme.dark.appColors.seedColor, brightness: Brightness.dark));
+    colorScheme: ColorScheme.dark(background: AppColors.veryDarkGrey));
