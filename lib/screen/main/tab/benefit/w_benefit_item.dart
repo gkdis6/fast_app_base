777ca07937cc914620dc7547520112b6c1c2fcff
SangYong <gkdis6@gmail.com>
2024-09-29 17:22:03 +0900

    benefit 탭 화면

---
+++ lib/screen/main/tab/benefit/w_benefit_item.dart
@@ -0,0 +1,34 @@
+import 'package:fast_app_base/common/dart/extension/context_extension.dart';
+import 'package:fast_app_base/common/widget/w_height_and_width.dart';
+import 'package:fast_app_base/screen/main/tab/benefit/vo_benefit.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class BenefitItem extends StatelessWidget {
+  final Benefit benefit;
+  const BenefitItem({super.key, required this.benefit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          benefit.imagePath,
+          width: 50,
+          height: 50,
+        ),
+        width10,
+        Column(
+          children: [
+            benefit.subTitle.text.size(13).make(),
+            height5,
+            benefit.title.text
+                .color(context.appColors.blueText)
+                .size(13)
+                .make(),
+          ],
+        )
+      ],
+    ).pSymmetric(v: 20);
+  }
+}
