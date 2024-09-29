498f257347dc37e02a6e807b68afd0a5af4e9fae
SangYong <gkdis6@gmail.com>
2024-09-29 17:21:43 +0900

    lessImportantText colorcode lessImportant 로 수정

--- lib/screen/notification/w_notification_item.dart
+++ lib/screen/notification/w_notification_item.dart
@@ -41,7 +41,7 @@ class _NotificationItemWidgetState extends State<NotificationItemWidget> {
                 ),
                 widget.notification.type.name.text
                     .size(12)
-                    .color(context.appColors.lessImportantText)
+                    .color(context.appColors.lessImportant)
                     .make(),
                 emptyExpanded,
                 timeago
@@ -49,7 +49,7 @@ class _NotificationItemWidgetState extends State<NotificationItemWidget> {
                         locale: context.locale.languageCode)
                     .text
                     .size(13)
-                    .color(context.appColors.lessImportantText)
+                    .color(context.appColors.lessImportant)
                     .make(),
                 width10,
               ],
